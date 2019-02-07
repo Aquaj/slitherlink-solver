36e2aacdf081a75bcb804975d2d5ab7f9d251d8b
Niclorix <camille.aussignac@gmail.com>
2019-02-07 19:54:11 +0100

    output modifications

--- slitherlink/src/output/outpuc.c
+++ slitherlink/src/output/outpuc.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define M 34
-#define N 21
+#define M 10
+#define N 10
 
 
 
@@ -10,13 +10,13 @@ int main(int argc, char * argv[])
 {
   //bord haut +
   printf("╔");
-  for(int i = 0 ; i<M+2 ; ++i)
+  for(int i = 0 ; i<M*2+3 ; ++i)
     printf("═");
   printf("╗\n");
 
   //bord haut
   printf("║┌");
-  for(int i = 0 ; i<M ; ++i)
+  for(int i = 0 ; i<M*2+1 ; ++i)
     printf("─");
   printf("┐║\n");
 
@@ -25,23 +25,32 @@ int main(int argc, char * argv[])
 
   //affichage du jeu
   for(int j = 0 ; j<N ; ++j){
-    printf("║│");
-    for(int i = 0; i<M; ++i)
-      printf(" ");
-    printf("│║\n");
+    printf("║│ ");
+    int i;
+    for(i=0; i<M-1; ++i)
+      printf("%d│", i);
+    printf("%d", i);
+    printf(" │║");
+    printf("\n");
+    printf("║│ ");
+    for(int i = 0; i<M-1; ++i)
+      printf("─.");
+    printf("─");
+    printf(" │║\n");
+
   }
 
 
 
   //bord bas
   printf("║└");
-  for(int i = 0 ; i<M ; ++i)
+  for(int i = 0 ; i<M*2+1 ; ++i)
     printf("─");
   printf("┘║\n");
 
   //bord bas+
   printf("╚");
-  for(int i = 0 ; i<M+2; ++i)
+  for(int i = 0 ; i<M*2+3; ++i)
     printf("═");
   printf("╝\n");
 }
