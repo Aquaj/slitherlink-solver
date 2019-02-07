4e8249466623d498ce3f57606cd2e4cf0224e1b7
Niclorix <camille.aussignac@gmail.com>
2019-02-07 19:28:21 +0100

    finished board edges

--- slitherlink/src/output/outpuc.c
+++ slitherlink/src/output/outpuc.c
@@ -10,13 +10,13 @@ int main(int argc, char * argv[])
 {
   //bord haut +
   printf("╔");
-  for(int i = 0 ; i<M-2 ; ++i)
+  for(int i = 0 ; i<M+2 ; ++i)
     printf("═");
   printf("╗\n");
 
   //bord haut
   printf("║┌");
-  for(int i = 0 ; i<M-4 ; ++i)
+  for(int i = 0 ; i<M ; ++i)
     printf("─");
   printf("┐║\n");
 
@@ -24,23 +24,24 @@ int main(int argc, char * argv[])
 
 
   //affichage du jeu
-  printf("║│");
-  for(int i = 0; i<N-4; ++i)
-    printf(" ");
-  printf("│║\n");
-
+  for(int j = 0 ; j<N ; ++j){
+    printf("║│");
+    for(int i = 0; i<N; ++i)
+      printf("%d", j);
+    printf("│║\n");
+  }
 
 
 
   //bord bas
   printf("║└");
-  for(int i = 0 ; i<M-4 ; ++i)
+  for(int i = 0 ; i<M ; ++i)
     printf("─");
   printf("┘║\n");
 
   //bord bas+
   printf("╚");
-  for(int i = 0 ; i<M-2 ; ++i)
+  for(int i = 0 ; i<M+2; ++i)
     printf("═");
   printf("╝\n");
 }
