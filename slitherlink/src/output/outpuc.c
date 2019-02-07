ee49e3c7bc8a4bc73ef4798cb042c66010209957
Niclorix <camille.aussignac@gmail.com>
2019-02-07 19:20:19 +0100

    finished the output of game bord edges

--- slitherlink/src/output/outpuc.c
+++ slitherlink/src/output/outpuc.c
@@ -4,44 +4,41 @@
 #define M 10
 #define N 10
 
-int addHorizontalEdge(char left, char middle, char right){
-  printf("%c", left);
-  for(int i = 0 ; i<M-2 ; ++i)
-    printf("%c", middle);
-  printf("%c\n", right);
-}
+
 
 int main(int argc, char * argv[])
 {
   //bord haut +
-  // /!\ VERIFIER POURQUOI CA MARCHE PAS SI ON RENTRE LE CODE ASCII
-  addHorizontalEdge(╔, ═, ╗);
+  printf("╔");
+  for(int i = 0 ; i<M-2 ; ++i)
+    printf("═");
+  printf("╗\n");
 
   //bord haut
-  printf("┌");
-  for(int i = 0 ; i<M-2 ; ++i)
+  printf("║┌");
+  for(int i = 0 ; i<M-4 ; ++i)
     printf("─");
-  printf("┐\n");
+  printf("┐║\n");
+
+
+
 
   //affichage du jeu
 
 
+
+
+
+
   //bord bas
-  printf("└");
-  for(int i = 0 ; i<M-2 ; ++i)
+  printf("║└");
+  for(int i = 0 ; i<M-4 ; ++i)
     printf("─");
-  printf("┘\n");
+  printf("┘║\n");
 
   //bord bas+
   printf("╚");
   for(int i = 0 ; i<M-2 ; ++i)
     printf("═");
   printf("╝\n");
-
-/*
-  for(int i =100 ; i < 300 ; ++i){
-    printf("%d = %c\n", i, i);
-  }
-  PREUVE QUE CA MARCHE PAS SUR LE TERMINAL
-  */
 }
