551914e2b8c8ec82a93a1a7c25d01659da5db637
Niclorix <camille.aussignac@gmail.com>
2019-02-08 16:28:44 +0100

    corrected case with too small grids

--- slitherlink/src/output/outpuc.c
+++ slitherlink/src/output/outpuc.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define M 5
-#define N 5
+#define M 10
+#define N 0
 
-
+//METTRE UN GENERATEUR AU PIF POUR TESTER
 
 int main(int argc, char * argv[])
 {
+  if(N<1 || M<1){
+    printf("The size of the grid must be over 1x1.\n");
+    return 1;
+  }
   //bord haut +
   printf("╔");
   for(int i = 0 ; i<M*2+5 ; ++i)
