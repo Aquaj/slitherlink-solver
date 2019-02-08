ef31138f56d3852522dcdc0979cac8bf26967429
Niclorix <camille.aussignac@gmail.com>
2019-02-08 16:17:58 +0100

    output works correctly, the grid shows now properly even with odds N or/and M

--- slitherlink/src/output/outpuc.c
+++ slitherlink/src/output/outpuc.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define M 10
-#define N 10
+#define M 5
+#define N 5
 
 
 
@@ -10,13 +10,13 @@ int main(int argc, char * argv[])
 {
   //bord haut +
   printf("╔");
-  for(int i = 0 ; i<M*2+3 ; ++i)
+  for(int i = 0 ; i<M*2+5 ; ++i)
     printf("═");
   printf("╗\n");
 
   //bord haut
   printf("║┌");
-  for(int i = 0 ; i<M*2+1 ; ++i)
+  for(int i = 0 ; i<M*2+3 ; ++i)
     printf("─");
   printf("┐║\n");
 
@@ -24,18 +24,22 @@ int main(int argc, char * argv[])
 
 
   //affichage du jeu
+  printf("║│ .");
+  for(int i = 0; i<M; ++i)
+    printf("─.");
+  printf(" │║\n");
+
   for(int j = 0 ; j<N ; ++j){
     printf("║│ ");
     int i;
-    for(i=0; i<M-1; ++i)
+    printf("│");
+    for(i=0; i<M; ++i)
       printf("%d│", i);
-    printf("%d", i);
     printf(" │║");
     printf("\n");
-    printf("║│ ");
-    for(int i = 0; i<M-1; ++i)
+    printf("║│ .");
+    for(int i = 0; i<M; ++i)
       printf("─.");
-    printf("─");
     printf(" │║\n");
 
   }
@@ -44,13 +48,13 @@ int main(int argc, char * argv[])
 
   //bord bas
   printf("║└");
-  for(int i = 0 ; i<M*2+1 ; ++i)
+  for(int i = 0 ; i<M*2+3 ; ++i)
     printf("─");
   printf("┘║\n");
 
   //bord bas+
   printf("╚");
-  for(int i = 0 ; i<M*2+3; ++i)
+  for(int i = 0 ; i<M*2+5; ++i)
     printf("═");
   printf("╝\n");
 }
