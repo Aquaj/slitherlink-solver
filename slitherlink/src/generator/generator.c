165533423b8921a6fbb871ceaaf1016976a5afe9
H3lly <h3lly.h0lly@gmail.com>
2019-02-11 21:38:39 +0100

    resolving conflicts

--- slitherlink/src/generator/generator.c
+++ slitherlink/src/generator/generator.c
@@ -20,8 +20,6 @@ void draw_all_values(struct map* my_map, struct graph* my_graph){
 void remove_values(struct map* my_map){
   map_remove_values(my_map);
 }
-<<<<<<< HEAD
-=======
 
 void fill_points(struct map* my_map){
   map_fill_points(my_map);
@@ -39,4 +37,3 @@ int main(){
 
   fill_points(my_map);
 }
->>>>>>> 1b93a248370bc1c6e92b63dc17037462d160761c
