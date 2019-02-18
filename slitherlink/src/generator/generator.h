ff0fa3293e9e72844a774985f14dde5566721346
Niclorix <camille.aussignac@gmail.com>
2019-02-18 20:58:53 +0100

    added missing declaration of function

--- slitherlink/src/generator/generator.h
+++ slitherlink/src/generator/generator.h
@@ -10,5 +10,6 @@ void draw_all_values(struct map* my_map, struct grid* my_grid);
 void draw_values(struct map* my_map);
 void remove_values(struct grid* my_grid);
 void fill_points(struct map* my_map);
+void print_bit(char a);
 
 #endif
